8404161882714eee5e0968890d1f59ff32a76fbc
Vadim Kantorov <vadimkantorov@gmail.com>
2016-09-02 20:11:10 +0200

    detection_mAP now works with octave

--- detection_mAP.lua
+++ detection_mAP.lua
@@ -3,7 +3,7 @@ dofile('util.lua')
 dofile('dataset.lua')
 threads = require 'threads'
 
-local MATLAB = assert((sys.execute('which matlab') and 'matlab -r') or (sys.execute('which octave') and 'octave --eval'), 'matlab or octave not found in PATH')
+local MATLAB = assert((#sys.execute('which matlab') > 0 and 'matlab -r') or (#sys.execute('which octave') > 0 and 'octave --eval'), 'matlab or octave not found in PATH')
 local subset = 'test'
 output_field = opts.OUTPUT_FIELDS[1]
 
@@ -30,10 +30,13 @@ end
 res = {[output_field] = {_mean = nil, by_class = {}}}
 APs = torch.FloatTensor(numClasses):zero()
 
+local imgsetpath = paths.tmpname()
+os.execute(('sed \'s/$/ -1/\' %s > %s'):format(paths.concat(opts.PATHS.VOC_DEVKIT_VOCYEAR, 'ImageSets', 'Main', subset .. '.txt'), imgsetpath)) -- hack for octave
+
 jobQueue = threads.Threads(numClasses)
 for classLabelInd, classLabel in ipairs(classLabels) do
 	jobQueue:addjob(function()
-		os.execute(('%s "oldpwd = pwd; cd(\'%s\'); addpath(fullfile(pwd, \'VOCcode\')); VOCinit; cd(oldpwd); VOCopts.testset = \'%s\'; VOCopts.detrespath = \'%s\'; classLabel = \'%s\'; [rec, prec, ap] = VOCevaldet(VOCopts, \'comp4\', classLabel, false); dlmwrite(sprintf(VOCopts.detrespath, \'resu4\', classLabel), ap); quit;"'):format(MATLAB, paths.dirname(opts.PATHS.VOC_DEVKIT_VOCYEAR), subset, detrespath, classLabel))
+		os.execute(('%s "oldpwd = pwd; cd(\'%s\'); addpath(fullfile(pwd, \'VOCcode\')); VOCinit; cd(oldpwd); VOCopts.testset = \'%s\'; VOCopts.detrespath = \'%s\'; VOCopts.imgsetpath = \'%s\'; classLabel = \'%s\'; [rec, prec, ap] = VOCevaldet(VOCopts, \'comp4\', classLabel, false); dlmwrite(sprintf(VOCopts.detrespath, \'resu4\', classLabel), ap); quit;"'):format(MATLAB, paths.dirname(opts.PATHS.VOC_DEVKIT_VOCYEAR), subset, detrespath, imgsetpath, classLabel))
 		return tonumber(io.open(detrespath:format('resu4', classLabel)):read('*all'))
 	end, function(ap) res[output_field].by_class[classLabel] = ap; APs[classLabelInd] = ap; end)
 end
